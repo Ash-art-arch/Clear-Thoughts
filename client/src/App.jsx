3f255f13e37994530766e008c54e4d70ccb48b5c
Ash-art-arch <120031662+Ash-art-arch@users.noreply.github.com>
2024-03-20 15:47:39 +0530

    Inital Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Nav from './components/Navbar/Nav'
+import Hero from './components/Hero/Hero'
+function App() {
+
+
+  return (
+    <div className='main'>
+     <Nav/>
+     <Hero/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Navbar/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Nav.css"
+const Nav = () => {
+  return (
+    <div className='nav'>
+      <h1>Clear Thought </h1>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default Nav
